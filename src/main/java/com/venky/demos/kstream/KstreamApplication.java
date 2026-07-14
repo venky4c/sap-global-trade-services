0277fe63b999c23f8abf0b6435bc986797e41f62
venkateswar buddiga <venky4c@gmail.com>
2026-07-14 14:05:18 +0530

    consolidated the main app class

--- src/main/java/com/venky/demos/kstream/KstreamApplication.java
+++ src/main/java/com/venky/demos/kstream/KstreamApplication.java
@@ -1,17 +1,23 @@
 package com.venky.demos.kstream;
 
-import org.apache.commons.logging.LogFactory;
+import com.venky.demos.kstream.config.KafkaStreamsProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = {
+        "org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration",
+        "org.springframework.boot.autoconfigure.kafka.KafkaStreamsAutoConfiguration"
+})
+@EnableConfigurationProperties(KafkaStreamsProperties.class)
 public class KstreamApplication {
+
     private static final Logger log = LoggerFactory.getLogger(KstreamApplication.class.getSimpleName());
+
     public static void main(String[] args) {
         log.info("Inside KstreamApplication >>>>>>>");
         SpringApplication.run(KstreamApplication.class, args);
     }
-
 }
